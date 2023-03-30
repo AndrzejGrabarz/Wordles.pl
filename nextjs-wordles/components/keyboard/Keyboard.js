241246aee5773b2cb33cc1bb5e306cdb5916f89e
AndrzejGrabarz <andgra01@gmail.com>
2023-03-30 21:25:10 +0200

    Keyboard_Komponentowanie_struktury

--- nextjs-wordles/components/keyboard/Keyboard.js
+++ nextjs-wordles/components/keyboard/Keyboard.js
@@ -1,61 +1,19 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from 'react'
+import KeyBoardRow from "./KeyBoardRow";
+
 
 function Keyboard() {
   
- 
+  const KEYBOARD_SIZE = [0,1,2,3]
+
   return (
     <div className='flex flex-col my-10'> 
         
-      <div className=" flex gap-2 my-1">
-          <div className="keyboard_block">Q</div>
-          <div className="keyboard_block">W</div>
-          <div className="keyboard_block">E</div>
-          <div className="keyboard_block">R</div>
-          <div className="keyboard_block">T</div>
-          <div className="keyboard_block">Y</div>
-          <div className="keyboard_block">U</div>
-          <div className="keyboard_block">I</div>
-          <div className="keyboard_block">O</div>
-          <div className="keyboard_block">P</div>
-      </div>
-        
-      <div className=" flex gap-2 my-1">
-          <div className="keyboard_block grow">A</div>
-          <div className="keyboard_block grow">S</div>
-          <div className="keyboard_block grow">D</div>
-          <div className="keyboard_block grow">F</div>
-          <div className="keyboard_block grow">G</div>
-          <div className="keyboard_block grow">H</div>
-          <div className="keyboard_block grow">J</div>
-          <div className="keyboard_block grow">K</div>
-          <div className="keyboard_block grow">L</div>
-      </div>
-        
-      <div className=" flex gap-2 my-1">
-          <div className="keyboard_block grow">Enter</div>
-          <div className="keyboard_block">Z</div>
-          <div className="keyboard_block">X</div>
-          <div className="keyboard_block">C</div>
-          <div className="keyboard_block">V</div>
-          <div className="keyboard_block">B</div>
-          <div className="keyboard_block">N</div>
-          <div className="keyboard_block">M</div>
-          <div className="keyboard_block grow">[ x ]</div>
-      </div>
-
-      <div className=" flex gap-2 my-1">
-          <div className="keyboard_block grow">Ą</div>
-          <div className="keyboard_block grow">Ć</div>
-          <div className="keyboard_block grow">Ę</div>
-          <div className="keyboard_block grow">Ł</div>
-          <div className="keyboard_block grow">Ń</div>
-          <div className="keyboard_block grow">Ó</div>
-          <div className="keyboard_block grow">Ś</div>
-          <div className="keyboard_block grow">Ź</div>
-          <div className="keyboard_block grow">Ż</div>
-      </div>
+      {KEYBOARD_SIZE.map((row)=>{
+        return <KeyBoardRow id={KEYBOARD_SIZE[row]}></KeyBoardRow>
+      })}
 
     </div>
   )
--- nextjs-wordles/components/keyboard/keyBoardRow.js
+++ nextjs-wordles/components/keyboard/keyBoardRow.js
@@ -1,28 +1,82 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from 'react'
+import classNames from 'classnames';
 import Key from "./Key";
 
-function keyBoardRow({id}) {
+function KeyBoardRow({id}) {
   
-let klawisze = new Array();
+  let litery_0 = new Array(9);
+ 
+  litery_0[0] = "Q";
+  litery_0[1] = "W";
+  litery_0[2] = "E";
+  litery_0[3] = "R";
+  litery_0[4] = "T";
+  litery_0[5] = "Y";
+  litery_0[6] = "U";
+  litery_0[7] = "I";
+  litery_0[8] = "O";
+  litery_0[9] = "P";
 
-if(id === 0) {
-  klawisze = litery_1
-}else if (id ===1) {
-  klawisze = litery_2
-}else if (id ===2) {
-  klawisze = litery_3
-}else if (id ===3) {
-  klawisze = litery_4
-}
+  let litery_1 = new Array(8);
+ 
+  litery_1[0] = "A";
+  litery_1[1] = "S";
+  litery_1[2] = "D";
+  litery_1[3] = "F";
+  litery_1[4] = "G";
+  litery_1[5] = "H";
+  litery_1[6] = "J";
+  litery_1[7] = "K";
+  litery_1[8] = "L";
+  
+
+  let litery_2 = new Array(8);
+ 
+  litery_2[0] = "Enter";
+  litery_2[1] = "Z";
+  litery_2[2] = "X";
+  litery_2[3] = "C";
+  litery_2[4] = "V";
+  litery_2[5] = "B";
+  litery_2[6] = "N";
+  litery_2[7] = "M";
+  litery_2[8] = "Delete";
+  
+
+  let litery_3 = new Array(8);
+ 
+  litery_3[0] = "Ą";
+  litery_3[1] = "Ć";
+  litery_3[2] = "Ę";
+  litery_3[3] = "Ł";
+  litery_3[4] = "Ń";
+  litery_3[5] = "Ó";
+  litery_3[6] = "Ś";
+  litery_3[7] = "Ź";
+  litery_3[8] = "Ż";
+
+  let RowId = [];
+  if(id ===0){
+    RowId = litery_0
+  }else if (id ===1){
+    RowId = litery_1
+  }else if (id ===2){
+    RowId = litery_2
+  }else if (id ===3){
+    RowId = litery_3
+  }
+
+ 
+
 
   return (
     <>
-       <div className=" flex gap-2 my-1" id={id}>
+       <div className=" flex gap-2 my-1">
 
-          {klawisze.map((key)=>{
-            return <Key rowID={id} litera={klawisze[key]}></Key>
+          {RowId.map((litera)=>{
+            return <Key keyId={litera} RowId = {id}></Key>
           })}
           
       </div>
@@ -30,4 +84,4 @@ if(id === 0) {
   )
 }
 
-export default keyBoardRow
+export default KeyBoardRow

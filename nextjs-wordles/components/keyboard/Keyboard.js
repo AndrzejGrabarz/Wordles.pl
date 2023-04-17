204f85d93227198a47c0ac27a0f43953d0848131
Adam Piskorek <adam.piskorek.pl@gmail.com>
2023-04-17 12:21:19 +0200

    clean up

--- nextjs-wordles/components/keyboard/Keyboard.js
+++ nextjs-wordles/components/keyboard/Keyboard.js
@@ -1,33 +1,36 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import KeyBoardRow from './keyBoardRow';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from 'react'
-import KeyBoardRow from "./keyBoardRow";
-
-
-function Keyboard({KeyboardKey, setKeyboardKey, specialKeyboardKey, setSpecialKeyboardKey}) {
-  
-  let KEYBOARD = [["Q", 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O','P'],
-                    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-                    ["Enter", 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Delete'],
-                    ['Ą', 'Ć', 'Ę', 'Ł', 'Ń', 'Ó', 'Ś', 'Ź', 'Ż']
-                  ]
+function Keyboard({
+  KeyboardKey,
+  setKeyboardKey,
+  specialKeyboardKey,
+  setSpecialKeyboardKey,
+}) {
+  let KEYBOARD = [
+    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
+    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
+    ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Delete'],
+    ['Ą', 'Ć', 'Ę', 'Ł', 'Ń', 'Ó', 'Ś', 'Ź', 'Ż'],
+  ];
 
   return (
-    <div className='flex flex-col my-10'> 
-        
-      {KEYBOARD.map((row)=>{
-        return <KeyBoardRow 
-        key = {row}
-        rowLine ={row}
-        KeyboardKey = {KeyboardKey}
-        setKeyboardKey = {setKeyboardKey}
-        specialKeyboardKey = {specialKeyboardKey}
-        setSpecialKeyboardKey = {setSpecialKeyboardKey}
-        />
+    <div className="flex flex-col my-10">
+      {KEYBOARD.map((row) => {
+        return (
+          <KeyBoardRow
+            key={row}
+            rowLine={row}
+            KeyboardKey={KeyboardKey}
+            setKeyboardKey={setKeyboardKey}
+            specialKeyboardKey={specialKeyboardKey}
+            setSpecialKeyboardKey={setSpecialKeyboardKey}
+          />
+        );
       })}
-
     </div>
-  )
+  );
 }
 
-export default Keyboard
+export default Keyboard;

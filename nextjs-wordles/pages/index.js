9a41aa7feb74a1301760980f7a785cf1d7544acb
Adam Piskorek <adam.piskorek.pl@gmail.com>
2023-04-17 14:33:21 +0200

    board does not show up

--- nextjs-wordles/pages/index.js
+++ nextjs-wordles/pages/index.js
@@ -6,12 +6,17 @@ const CORRECT_WORD = 'GORYL';
 
 const SPECIAL_KEYS = ['Enter', 'Delete'];
 
+// 
 const ROW_COUNT = 5;
+const COL_COUNT = 5;
+
+const DEFAULT_STATE = ['', '', '', '', '', ''];
 
 export default function Home() {
-  const [board, setBoardState] = useState(['', '', '', '', '', '']);
+  let [board, setBoardState] = useState(DEFAULT_STATE);
 
   const [keyboardKey, setKeyboardKey] = useState('');
+  const [currentRow, setCurrentRow] = useState(0);
 
   const isSpecialKey = (key) => SPECIAL_KEYS.includes(key);
 
@@ -24,6 +29,7 @@ export default function Home() {
       console.log('Naciśnięto klawisz: ' + event.key);
       setKeyboardKey(event.key.toUpperCase());
     };
+
     document.addEventListener('keypress', handleKeyPress);
   });
 
@@ -32,16 +38,33 @@ export default function Home() {
   //======================================================
 
   useEffect(() => {
-    if (!isSpecialKey(keyboardKey)) updateBoard();
+    if (!isSpecialKey(keyboardKey)) {
+      updateBoard(keyboardKey);
+    } else {
+      verifyState();
+    }
   }, [keyboardKey]);
 
-  const updateBoard = () => {
-    console.log(board);
-    if (board[0].length < 5) {
-      board[0] = board[0] + keyboardKey;
-      setBoardState(board);
+  const updateBoard = (key) => {
+    let updatedBoard = [...board];
+
+    if (board[currentRow].length < 5) {
+      updatedBoard[currentRow] = board[currentRow] + key;
     }
+
+    setBoardState(updatedBoard);
   };
+
+  function verifyState() {
+    if (keyboardKey === 'Enter' && isWordCorrect()) {
+      communicateState('win');
+    }
+    if (currentRow === 4 && keyboardKey === 'Enter' && !isWordCorrect()) {
+      communicateState('lose');
+    }
+
+    setCurrentRow(currentRow + 1);
+  }
 
   //======================================================
   // Sprawdzenie checkWord
@@ -53,10 +76,15 @@ export default function Home() {
       ^Funkcja odpala się tylko przy zmianie stanu w specialKeyboardKey guzikami zpecjalnymi które zmieniają ten stan to "Enter" i "Delet"
    */
 
-  function checkWord() {
-    if (board[0] === CORRECT_WORD) {
+  function isWordCorrect() {
+    return board[currentRow] === CORRECT_WORD;
+  }
+
+  function communicateState(stateName) {
+    if (stateName === 'win') {
       alert('Wygrałeś');
-    } else {
+    }
+    if (stateName === 'lose') {
       alert('Nie wygrałeś');
     }
   }

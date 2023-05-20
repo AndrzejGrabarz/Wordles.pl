5e9a9f50b9d646d8a4d7a11ce19eb0ea6c3e872b
Adam Piskorek <adam.piskorek.pl@gmail.com>
2023-05-20 15:46:43 +0200

    Linter, heroicons version, cleanup

--- nextjs-wordles/next.config.js
+++ nextjs-wordles/next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
-
-
-// module.exports = {
-//   reactStrictMode: false,
-//   webpack5: true,
-//   webpack: (config) => {
-//     config.resolve.fallback = { fs: false };
-//     return config
-//   },
-// };
+module.exports = nextConfig;
